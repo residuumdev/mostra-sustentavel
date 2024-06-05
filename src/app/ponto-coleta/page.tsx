9dd863c64c99af4c7fa391de7982fe3c11781cb3
Cristian Garcia <garcia.ac.cristian@gmail.com>
2024-06-05 13:25:31 -0400

    feat: adiciona placeholder no input de telefone.

--- src/app/ponto-coleta/page.tsx
+++ src/app/ponto-coleta/page.tsx
@@ -54,6 +54,7 @@ export default function HomeComponent() {
             type="number"
             name="telefone"
             id="telefone"
+            placeholder="(DDD) 0000-0000"
             className="focus:ring-primary-600 focus:border-primary-600    block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900"
           />
         </div>
